3a8b30868d50f5cfe391106f6a983aea77c7273a
Vanek <vanevo00@gmail.com>
2021-02-14 12:05:17 +0100

    feat: betted short date format

--- client/utils/getShortDate.ts
+++ client/utils/getShortDate.ts
@@ -1,6 +1,6 @@
 const getShortDate = (date: string): string => {
   const dateObject = new Date(date)
-  return `${dateObject.getDate()}.${dateObject.getMonth() + 1}.${String(dateObject.getFullYear()).slice(-2)}`
+  return `${dateObject.getDate()}. ${dateObject.getMonth() + 1}. ${String(dateObject.getFullYear())}`
 }
 
 export default getShortDate
