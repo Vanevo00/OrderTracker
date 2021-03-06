963e025fafd465ad10a5d8f4fb6233517fd7a67a
Vanek <vanevo00@gmail.com>
2021-03-06 21:55:01 +0100

    feat: orders scrollable

--- client/components/MainContentWrapper.tsx
+++ client/components/MainContentWrapper.tsx
@@ -3,7 +3,7 @@ import { Col } from 'react-bootstrap'
 import MainContent from './MainContent'
 
 const MainContentWrapper = () => (
-  <Col xl={9} md={8} sm={7} className='d-none d-sm-block white-background height-remainder p-5'>
+  <Col xl={9} md={8} sm={7} className='d-none d-sm-block white-background min-height-remainder p-5'>
     <MainContent/>
   </Col>
 )
