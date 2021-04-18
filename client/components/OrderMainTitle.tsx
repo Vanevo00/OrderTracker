0e1a5219d8d49dd8058b13320dfdc1721f91e694
Vanek <vanevo00@gmail.com>
2021-04-18 10:41:47 +0200

    feat: smaller font for mainTitle on mobile

--- client/components/OrderMainTitle.tsx
+++ client/components/OrderMainTitle.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { IOrderPopulated } from '../../types/Order'
 import formatDate from '../utils/formatDate'
 import { RootStateOrAny, useSelector } from 'react-redux'
+import { DEVICE_MOBILE } from '../../common/devices'
 
 const OrderMainTitle = () => {
   const {
-    activeOrder,
-    orderSaveStatus
-  } = useSelector((state: RootStateOrAny) => state.orderState)
+    orderState: {
+      activeOrder,
+      orderSaveStatus
+    },
+    deviceState: {
+      device
+    }
+  } = useSelector((state: RootStateOrAny) => state)
 
   const generateMainTitle = (order: IOrderPopulated): string => {
     if (!order.name) return '[ bez názvu ]'
@@ -30,7 +36,11 @@ const OrderMainTitle = () => {
 
   return (
     <div>
-      <h3 className='m-0 small-line-height'><strong>{generateMainTitle(activeOrder)}</strong></h3>
+      {
+        device === DEVICE_MOBILE
+          ? <h5 className='m-0 small-line-height'><strong>{generateMainTitle(activeOrder)}</strong></h5>
+          : <h3 className='m-0 small-line-height'><strong>{generateMainTitle(activeOrder)}</strong></h3>
+      }
       <p className='gray-font'><small>{orderSaveStatus ? orderSaveStatus : `poslední aktualizace: ${formatDate(activeOrder.updated, true)}`}</small></p>
     </div>
   )
